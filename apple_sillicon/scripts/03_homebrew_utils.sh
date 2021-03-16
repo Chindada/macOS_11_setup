739eb38e771d568cab456c1ee43cdd44556fd011
TimHsu@MBP <maochindada@gmail.com>
2021-03-17 00:48:06 +0800

    # grep "^plugins=(git" ~/.zshrc

--- apple_sillicon/scripts/03_homebrew_utils.sh
+++ apple_sillicon/scripts/03_homebrew_utils.sh
@@ -6,17 +6,19 @@ git config --global user.email "maochindada@gmail.com"
 
 brew install gnu-sed
 gsed -i '1 i ZSH_DISABLE_COMPFIX="true"' ~/.zshrc
-grep "^plugins=(git" ~/.zshrc
+
+# grep "^plugins=(git" ~/.zshrc
 gsed -i "/^plugins=(git/c plugins=(git colored-man-pages python brew osx)" ~/.zshrc
 
 brew install zsh-autosuggestions
 gsed -i '$ a source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh' ~/.zshrc
+
 brew install zsh-syntax-highlighting
 gsed -i '$ a source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' ~/.zshrc
 
 brew install --cask iterm2
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-grep "^ZSH_THEME" ~/.zshrc
+# grep "^ZSH_THEME" ~/.zshrc
 gsed -i '/^ZSH_THEME/c ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
 
 
@@ -34,20 +36,19 @@ brew install --cask sourcetree
 
 brew install --cask anydesk
 brew install --cask skype
-# brew install --cask google-chrome
+brew install --cask google-chrome
 
 brew install --cask homebrew/cask-versions/adoptopenjdk8
 brew install --cask android-sdk
+
 gsed -i '$ a export ANDROID_SDK_ROOT="/opt/homebrew/share/android-sdk"' ~/.zshrc
 
 sdkmanager
 touch ~/.android/repositories.cfg
 sdkmanager --update
 
-sdkmanager "build-tools;30.0.3"
+sdkmanager "build-tools;31.0.0-rc1"
 sdkmanager "platforms;android-30"
 sdkmanager "platform-tools"
 
 sudo gem install cocoapods
-
-
