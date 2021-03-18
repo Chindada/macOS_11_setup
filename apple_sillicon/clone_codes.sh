9bcbb7d80a6aafac9de40109add310617c261ab6
TimHsu@MBP <maochindada@gmail.com>
2021-03-18 12:58:48 +0800

    rearrange

--- apple_sillicon/clone_codes.sh
+++ apple_sillicon/clone_codes.sh
@@ -6,6 +6,7 @@ git clone git@192.168.10.239:R5012/iom_r5012_build_windows.git ~/dev_projects/mi
 
 # golang
 git clone git@github.com:Chindada/handyboss.git ~/dev_projects/golang/handyboss
+git clone git@github.com:Chindada/golang_leetcode.git ~/dev_projects/golang/golang_leetcode
 
 # flutter
 git clone git@github.com:Chindada/fake_iom.git ~/dev_projects/flutter/fake_iom
---
+++ apple_sillicon/install_01.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./scripts/01_homebrew.sh && \
+./scripts/02_ohmyzsh.sh && \
