31f3d8145a5f8ad129bf25f158fa2c5b2a564db5
TimHsu@M1BP <maochindada@gmail.com>
2021-04-14 12:40:56 +0800

    unzip -q

--- apple_sillicon/07_flutter.sh
+++ apple_sillicon/07_flutter.sh
@@ -4,6 +4,6 @@
 version="2.0.4"
 
 curl -fSL https://storage.googleapis.com/flutter_infra/releases/stable/macos/flutter_macos_$version-stable.zip --output flutter.zip
-unzip flutter.zip -d /Users/$USER/flutter_sdk
+unzip -q flutter.zip -d /Users/$USER/flutter_sdk
 rm -rf flutter.zip
 
